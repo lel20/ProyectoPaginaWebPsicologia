6b6249e3d70baa3c756a3192710c44b86b6741f4
lel20 <leonardoparedes.v.r@gmail.com>
2023-11-12 22:07:22 +0100

    Agregando Estilos

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
 #Librerías
 from typing import Union
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
+app = FastAPI() #Se invoca a la clase FastApi()
 
-app = FastAPI()
-
+#Se define una variable que indique donde se encuentran las plantillas
+template =Jinja2Templates(directory="templates")
+app.mount("/static", StaticFiles(directory="static"), name="static")
 @app.get("/")
-def presntar():
-    return "Hola mundo"
+def raiz(request: Request):
+    return template.TemplateResponse("index.html",{"request":request})
+@app.get("/sobremi")
+def sobreMi(request: Request):
+    return template.TemplateResponse("sobreMi.html",{"request":request})
